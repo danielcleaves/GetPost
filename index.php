76b0160d86a22043a04d69635b9bb841df6035bd
Daniel Cleaves <danielcleaves@actioncoach.com>
2015-11-08 17:32:52 -0600

    added

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset ="UTF-8">
+		<title>Document</title>
+	</head>
+	<body>
+		<form action ="some_page.php" method = "post">
+			First-Name: <input type = "text" name = "first_name">
+			Last-Name: <input type = "text" name = "last_name">
+			E-mail Address: <input type = "text" name = "email">
+			<button><input type ="submit" value "add user!">Add User</input></button>
+		</form>
+	</body>
+</html>
