0f153b23da9f6439bbf97499a93bc7dadc93e41a
Daniel Cleaves <danielcleaves@actioncoach.com>
2015-11-08 17:33:50 -0600

    addeD

---
+++ some_page.php
@@ -0,0 +1,4 @@
+<p> Your name is: <?php echo $_POST ["first_name"]; ?> </p>
+<p> Your last name is: <?php echo $_POST ["last_name"]; ?> </p>
+<p> Your email address is <?php echo $_POST ["email"]; ?> </p>
+
